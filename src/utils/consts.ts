f7fa9a6b2eac03637f96e2c11c0800fcf66f63d8
hoifong <hoi_fong@126.com>
2019-11-18 10:57:57 +0800

    修改七牛云绑定域名

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -12,7 +12,7 @@ export const APIS = {
 
 export const BANNER_SLIDE_SPEED = 3;
 
-export const QINIU_DOMAIN = 'pyfenxfkd.bkt.clouddn.com';
+export const QINIU_DOMAIN = 'album.xiached.com';
 
 export const HIDE_CONTROLLER_IN_SECONDS = 2;
 export const HIDE_TOAST_IN_SECONDS = 2;
